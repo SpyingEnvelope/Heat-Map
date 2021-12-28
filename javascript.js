ddef1e0f8405e0c3b26768d8c9bf0f8517dfd082
SpyingEnvelope <gadi800@gmail.com>
2021-12-28 08:11:19 -0700

    Added axis y and x

---
+++ javascript.js
@@ -0,0 +1,54 @@
+let xScale;
+let yScale;
+let svg;
+
+const w = 1000;
+const h = 500;
+const padding = 60;
+const yScaleHeight = h - padding;
+
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
+     .then(response => response.json())
+     .then(function(response) {
+         console.log(response);
+         addSvg(response);
+     })
+
+
+const addSvg = (response) => {
+    svg = d3.select('#graph-div')
+            .append('svg')
+            .attr('width', w)
+            .attr('height', h);
+
+    addScale(response);
+}
+
+const addScale = (response) => {
+    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    xScale = d3.scaleLinear()
+               .domain([1753, 2015])
+               .range([padding, w - padding]);
+
+    yScale = d3.scaleBand()
+               .domain(monthArray)
+               .range([padding, h - padding]);
+    
+    addAxis(response);
+}
+
+const addAxis = (response) => {
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append('g')
+       .attr('id','x-axis')
+       .attr('transform', `translate (0, ${yScaleHeight})`)
+       .call(xAxis.tickFormat(d => d));
+    
+    svg.append('g')
+       .attr('id', 'y-axis')
+       .attr('transform', `translate(${padding}, 0)`)
+       .call(yAxis);
+}
